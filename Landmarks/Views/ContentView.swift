571d35d347f7782e3e0962a752e8fe27bbd7cad8
urba-storage <urbastorage@gmail.com>
2023-01-23 15:06:02 -0800

    first commit contains list, details, images and graphs

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Maaz Adil on 1/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
